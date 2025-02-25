bfd87f45cc677b21480fbd0289d086fc3e08af9a
tamcony <tamcony@icloud.com>
2025-02-25 23:27:32 +0800

    feat: 添加两数相加和查找和最小的 K 对数字问题的解决方案

---
+++ src/problems/373.查找和最小的 K 对数字.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=373 lang=javascript
+ * @lcpr version=30006
+ *
+ * [373] 查找和最小的 K 对数字
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number} k
+ * @return {number[][]}
+ */
+var kSmallestPairs = function(nums1, nums2, k) {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,7,11]\n[2,4,6]\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,1,2]\n[1,2,3]\n2\n
+// @lcpr case=end
+
+ */
+
